01a289dfaaa0eb24b65ae7a0ab64012526c28597
unknown <nagarajuontoori@gmail.com>
2021-04-08 13:39:46 +0530

    my profile

---
+++ src/Component1.jsx
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+class Component1 extends Component{
+    render(){
+     return(
+       <div>
+
+      <div className="side-info1">
+        <span>
+        <p>If you don't want a man unhappy politically, don't give him two sides to a question to worry him; give him one. Better yet, give him none. Let him forget there is such a thing as war. If the government is inefficient, top-heavy, and tax-mad, better it be all those than that people worry over it. Peace, Montag. Give the people contests they win by remembering the words to more popular songs or the names of state capitals or how much corn Iowa grew last year. Cram them full of noncombustible data, chock them so damned full of 'facts' they feel stuffed, but absolutely 'brilliant' with information. Then they'll feel they're thinking, they'll get a sense of motion without moving. And they'll be happy, because facts of that sort don't change.”
+― Ray Bradbury, Fahrenheit 451</p></span><br/>
+          </div>
+          <div className="side-info2">
+          <span><p>If you don't want a man unhappy politically, don't give him two sides to a question to worry him; give him one. Better yet, give him none. Let him forget there is such a thing as war. If the government is inefficient, top-heavy, and tax-mad, better it be all those than that people worry over it. Peace, Montag. Give the people contests they win by remembering the words to more popular songs or the names of state capitals or how much corn Iowa grew last year. Cram them full of noncombustible data, chock them so damned full of 'facts' they feel stuffed, but absolutely 'brilliant' with information. Then they'll feel they're thinking, they'll get a sense of motion without moving. And they'll be happy, because facts of that sort don't change.”
+― Ray Bradbury, Fahrenheit 451</p>
+      
+          </span>
+        </div>
+        </div>
+         
+     );
+    }
+}
+
+
+export default Component1;
